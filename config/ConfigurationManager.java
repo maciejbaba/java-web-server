b78e13a122216932e690c7e29d329124eda1516d
maciek367 <maciek2baba2@gmail.com>
2023-04-27 23:27:47 +0200

    add methods

--- config/ConfigurationManager.java
+++ config/ConfigurationManager.java
@@ -3,6 +3,7 @@ package config;
 public class ConfigurationManager {
   
   private static ConfigurationManager myConfigurationManager; // singleton, only one instance of this class is needed
+  private static Configuration myCurrentConfiguration; // singleton, current configuration
 
   private ConfigurationManager() {
   }
@@ -14,5 +15,11 @@ public class ConfigurationManager {
     return myConfigurationManager;
   }
 
-  
+  public void loadConfigurationFile(String filePath) {
+    // load configuration from file
+  }
+
+  public void getCurrentConfiguration() {
+    // get current configuration
+  }
 }
